8daf110b724bb923838914c3c21da989739cef79
navdeep-stack <navdeepbajwa0034@gmail.com>
2022-08-22 12:59:10 -0400

    Update Add Post

--- JobKart/ViewControllers/AddPostVC.swift
+++ JobKart/ViewControllers/AddPostVC.swift
@@ -6,6 +6,7 @@ import UIKit
 
 class AddPostVC: UIViewController {
 
+    @IBOutlet weak var lblTitle: UILabel!
     @IBOutlet weak var txtname: UITextField!
     @IBOutlet weak var txtOAddress: UITextField!
     @IBOutlet weak var txtEmail: UITextField!
@@ -15,9 +16,11 @@ class AddPostVC: UIViewController {
     @IBOutlet weak var txtDescription: UITextField!
     @IBOutlet weak var txtRequirments: UITextField!
     @IBOutlet weak var btnPost: BlueThemeButton!
+    @IBOutlet weak var btnEditPost: BlueThemeButton!
     
     var flag = true
-    
+    var isEdit: Bool = false
+    var data: PostModel!
     
     private func validation(name: String, oAddress:String, email:String, Otype:String, address:String, salary:String ,description:String, requirement:String ) -> String {
         if name.isEmpty{
@@ -48,7 +51,17 @@ class AddPostVC: UIViewController {
             
             if error.isEmpty {
                 self.flag = false
-                self.addCreateData(name: self.txtname.text?.trim() ?? "", oAddress: self.txtOAddress.text?.trim() ?? "", email: self.txtEmail.text?.trim() ?? "", Otype: self.txtOType.text?.trim() ?? "", address: self.txtAddress.text?.trim() ?? "", salary: self.txtSalary.text?.trim() ?? "", description: self.txtDescription.text?.trim() ?? "", requirement: self.txtRequirments.text?.trim() ?? "", user_Email: GFunction.user.email)
+                self.addCreateData(name: self.txtname.text?.trim() ?? "", oAddress: self.txtOAddress.text?.trim() ?? "", email: self.txtEmail.text?.trim() ?? "", Otype: self.txtOType.text?.trim() ?? "", address: self.txtAddress.text?.trim() ?? "", salary: self.txtSalary.text?.trim() ?? "", description: self.txtDescription.text?.trim() ?? "", requirement: self.txtRequirments.text?.trim() ?? "", user_Email: GFunction.user.email,phone: GFunction.user.mobile)
+            }else{
+                Alert.shared.showAlert(message: error, completion: nil)
+            }
+        } else if sender == btnEditPost {
+            let error = self.validation(name: self.txtname.text?.trim() ?? "", oAddress: self.txtOAddress.text?.trim() ?? "", email: self.txtEmail.text?.trim() ?? "", Otype: self.txtOType.text?.trim() ?? "", address: self.txtAddress.text?.trim() ?? "", salary: self.txtSalary.text?.trim() ?? "", description: self.txtDescription.text?.trim() ?? "", requirement: self.txtRequirments.text?.trim() ?? "")
+            
+            if error.isEmpty {
+                self.flag = false
+                self.updateData(dataID: self.data.docID, name: self.txtname.text?.trim() ?? "", oAddress: self.txtOAddress.text?.trim() ?? "", email: self.txtEmail.text?.trim() ?? "", Otype: self.txtOType.text?.trim() ?? "", address: self.txtAddress.text?.trim() ?? "", salary: self.txtSalary.text?.trim() ?? "", description: self.txtDescription.text?.trim() ?? "", requirement: self.txtRequirments.text?.trim() ?? "", user_Email: GFunction.user.email)
+                
             }else{
                 Alert.shared.showAlert(message: error, completion: nil)
             }
@@ -57,23 +70,38 @@ class AddPostVC: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        self.btnPost.isHidden = false
+        self.btnEditPost.isHidden = true
+        if self.isEdit {
+            self.lblTitle.text = "Edit Job"
+            self.btnPost.isHidden = true
+            self.btnEditPost.isHidden = false
+            self.txtname.text = data.job_name.description
+            self.txtAddress.text = data.address.description
+            self.txtEmail.text = data.job_email.description
+            self.txtOType.text = data.job_oType.description
+            self.txtSalary.text = data.job_salary.description
+            self.txtDescription.text = data.description.description
+            self.txtRequirments.text = data.requirement.description
+            self.txtOAddress.text = data.job_address.description
+        }
         // Do any additional setup after loading the view.
     }
     
-    func addCreateData(name: String, oAddress:String, email:String, Otype:String, address:String, salary:String ,description:String, requirement:String, user_Email: String) {
+    func addCreateData(name: String, oAddress:String, email:String, Otype:String, address:String, salary:String ,description:String, requirement:String, user_Email: String, phone: String) {
         var ref : DocumentReference? = nil
         ref = AppDelegate.shared.db.collection(jJobs).addDocument(data:
                                                                             [
                                                                                 jJobAddress: oAddress,
                                                                                 jPostName : name,
-                                                                                jJobOType: Otype,
+                                                                                jOrgType: Otype,
                                                                                 jJobEmail: email,
-                                                                                jAddress: address,
+                                                                                jLocation: address,
                                                                                 jJobSalary : salary,
-                                                                                jDescription : description,
+                                                                                jJobDescription : description,
                                                                                 jRequirement : requirement,
-                                                                                jUserEmail : user_Email,
+                                                                                jEmpEmail : user_Email,
+                                                                                jPhone : phone,
                                                                                 jUID: GFunction.user.docID
                                                                             ])
         {  err in
@@ -85,6 +113,32 @@ class AddPostVC: UIViewController {
                     if Bool {
                         self.navigationController?.popViewController(animated: true)
                     }
+                }
+            }
+        }
+    }
+    
+    func updateData(dataID: String,name: String, oAddress:String, email:String, Otype:String, address:String, salary:String ,description:String, requirement:String, user_Email: String) {
+        let ref = Firestore.firestore().collection(jJobs).document(dataID)
+        ref.updateData([
+            jJobAddress: oAddress,
+            jPostName : name,
+            jJobOType: Otype,
+            jJobEmail: email,
+            jAddress: address,
+            jJobSalary : salary,
+            jDescription : description,
+            jRequirement : requirement,
+            jUserEmail : user_Email,
+            jUID: GFunction.user.docID
+        ]){ err in
+            if let err = err {
+                print("Error updating document: \(err)")
+                self.navigationController?.popViewController(animated: true)
+            } else {
+                print("Document successfully updated")
+                Alert.shared.showAlert(message: "Your profile has been updated successfully !!!") { (true) in
+                    self.navigationController?.popViewController(animated: true)
                 }
             }
         }
